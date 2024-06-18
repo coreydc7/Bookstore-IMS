30b1da610f460a41039ee99f1ea691bc05f65854
Corey Collins <corey.dc@outlook.com>
2024-06-17 19:19:21 -0600

    Implemented basic login page with POST request handling functionality.

---
+++ demoapp/src/main/webapp/index.jsp
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="description"
+        content="A small program to manage the Inventory of any Bookstore or avid book collector. This is a technical demonstration.">
+    <meta name="keywords" content="HTML,CSS,JavaScript,Java,MySQL,Apache Tomcat,Servlets">
+    <meta name="author" content="Corey Collins">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bookstore Inventory Management System</title>
+    <script src="./script.js"></script>
+    <link href="./stylesheets/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Cardo:400,700|Oswald" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <nav class="main-navbar">
+            <a href="index.jsp">Home</a>
+            <a href="inventory.jsp">Inventory</a>
+            <a href="about.jsp">Contact</a>
+            <a href="login.jsp">Log In</a>
+            <a href="register.jsp">Register</a>
+        </nav>
+    </header>
+    <div id="main-homepage">
+        <h1>Bookstore Inventory Management System</h1>
+        <br>
+        <h3>Search for a book!</h3>
+        <input type="text" id="bookSearchInput" placeholder="Enter a Book's name, author, year of publication, etc.">
+        <button onclick="searchBooks()">Search</button>
+        <div id="searchBooksResults"></div>
+    </div>
+
+</body>
+
+</html>
--- demoapp/src/main/webapp/script.js
+++ demoapp/src/main/webapp/script.js
@@ -1,16 +1,20 @@
 function searchBooks() {
     var searchInput = document.getElementById('bookSearchInput').value.trim();
     
+    // Send a GET request using the Fetch API
     fetch('/demoapp/searchBooks?userInput=' + searchInput)
     .then(response => {
         if (!response.ok) {
+            // Handles HTTP errors
             throw new Error('Network response was not OK');
         }
-        return response.json();
+        return response.json(); // Parses the JSON Response
     })
     .then(data => {
+        // Handles the response data
         displaySearchBookResult(data);
     })
+    // Handles Fetch API errors
     .catch(error => console.error('Error:', error));
 }
 
@@ -29,5 +33,56 @@ function displaySearchBookResult(books) {
             bookList.appendChild(listItem);
         });
         searchResultDiv.appendChild(bookList);
+    }
+}
+
+function login() {
+    var usernameInput = document.getElementById('usernameInput').value.trim();
+    var passwordInput = document.getElementById('passwordInput').value.trim();
+
+    // Creates an object to hold the entered login details
+    const loginData = {
+        username: usernameInput,
+        password: passwordInput
+    };
+
+    // Send a POST request using Fetch API
+    fetch('/demoapp/login', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        // Converts the JS Object into a JSON String
+        body: JSON.stringify(loginData)
+    })
+    .then(response => {
+        if(!response.ok) {
+            // Handles any HTTP errors
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json(); // Parses the JSON response
+    })
+    .then(data => {
+        // Handles the response data
+        handleLoginAttempt(data);
+    })
+    // Handles any Fetch API errors. 
+    .catch(error => console.error('Error:',error));
+}
+
+function handleLoginAttempt(data) {
+    var loginResultDiv = document.getElementById('loginResult');
+    loginResultDiv.innerHTML = "";
+
+    if(data.length === 0) {
+        loginResultDiv.textContent = "No members found."
+    } else {
+        var resultList = document.createElement('ul');
+        data.forEach(member => {
+            var listItem = document.createElement('li');
+            listItem.textContent = member.username;
+            resultList.appendChild(listItem);
+        });
+        loginResultDiv.appendChild(resultList);
     }
 }
