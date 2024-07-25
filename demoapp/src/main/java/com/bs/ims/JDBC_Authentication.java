599e7cceddfffd565b7fa3e219c5e3fd818c6826
Corey Collins <corey.dc@outlook.com>
2024-07-25 17:35:59 -0600

    Updated pom.xml

--- demoapp/src/main/java/com/bs/ims/JDBC_Authentication.java
+++ demoapp/src/main/java/com/bs/ims/JDBC_Authentication.java
@@ -4,8 +4,8 @@ public class JDBC_Authentication {
     // Populate these fields with Database Connection information for JDBC
 
     private static final String JDBC_URL = "jdbc:mysql://faure.cs.colostate.edu:3306/coreydc";
-    private static final String JDBC_USERNAME = "coreydc";
-    private static final String JDBC_PASSWORD = "835870365";
+    private static final String JDBC_USERNAME = "";
+    private static final String JDBC_PASSWORD = "";
 
     public String getURL() {
         return JDBC_URL;
